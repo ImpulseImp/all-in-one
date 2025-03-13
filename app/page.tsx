be3d3b39f014fce180dab2859097e7a0e640cce7
Impulse <impulses4mini@gmail.com>
2025-03-13 20:25:21 +0300

    add .prettierrc

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import EmailSubscribe from '@/components/email-subscribe/EmailSubscribe';
+
 export default function Home() {
-  return <h1>Hello Impulse</h1>;
+  return (
+    <div>
+      {/* <h1>Hello Impulse</h1> */}
+      <EmailSubscribe />
+    </div>
+  );
 }
---
+++ components/email-subscribe/EmailSubscribe.tsx
@@ -0,0 +1,4 @@
+function EmailSubscribe() {
+  return <div className='gi'></div>;
+}
+export default EmailSubscribe;
