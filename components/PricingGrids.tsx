4597eb4f882f2cb5e254124aa357a8d7358f9b05
Impulse <impulses4mini@gmail.com>
2025-03-14 11:36:10 +0300

    complete basic styling

--- components/PricingGrids.tsx
+++ components/PricingGrids.tsx
@@ -1,26 +1,31 @@
 function PricingGrids() {
   return (
     //Background Container
-    <div className="flex min-h-screen items-center justify-center bg-blue-950">
+    <div className="flex min-h-screen items-center justify-center bg-blue-950 font-sans text-white">
       {/* Cards Container */}
-      <div className="flex flex-col items-center justify-center md:flex-row">
+      <div className="mt-10 flex flex-col items-center justify-center space-y-4 md:flex-row md:space-y-0 md:space-x-4">
         {/* Card 1*/}
         <div className="rounded-xl bg-slate-700 text-white">
           {/* upper card container */}
-          <div className="mx-2 mt-2 bg-blue-950 px-2">
-            <h3>basic</h3>
-            <span>100GB</span>
-            <p>$1.99/month</p>
-            <a href="#">Purchase</a>
+          <div className="mx-2 mt-2 flex flex-col items-center justify-center rounded-t-xl bg-blue-950 p-8">
+            <div className="mb-10 uppercase">basic</div>
+            <span className="mb-2 font-serif text-5xl">100GB</span>
+            <p className="mb-4 font-light">$1.99/month</p>
+            <a
+              href="#"
+              className="my-6 inline-block rounded-lg border border-violet-600 px-10 py-3 text-center duration-200 hover:border-violet-800 hover:bg-violet-800"
+            >
+              Purchase
+            </a>
           </div>
           {/* Border */}
           <div className="border-t border-slate-700"></div>
           {/* lower card container */}
           <div className="mx-2 mb-2 bg-blue-950 p-4">
             {/* List Container */}
-            <div>
+            <div className="space-y-2">
               {/* List Item 1 */}
-              <div className="flex justify-center">
+              <div className="flex justify-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -34,10 +39,10 @@ function PricingGrids() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M5 12l5 5l10 -10" />
                 </svg>
-                <span className="ml-1 text-sm">100 GB of storage</span>
+                <span className="text-sm">100 GB of storage</span>
               </div>
               {/* List Item 2 */}
-              <div className="flex justify-center">
+              <div className="flex justify-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -51,10 +56,10 @@ function PricingGrids() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M5 12l5 5l10 -10" />
                 </svg>
-                <span className="ml-1 text-sm">100 GB of storage</span>
+                <span className="text-sm">Option to add members</span>
               </div>
               {/* List Item 3 */}
-              <div className="flex justify-center">
+              <div className="flex justify-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -68,7 +73,7 @@ function PricingGrids() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M5 12l5 5l10 -10" />
                 </svg>
-                <span className="ml-1 text-sm">100 GB of storage</span>
+                <span className="text-sm">Extra member benefits</span>
               </div>
             </div>
           </div>
@@ -76,20 +81,25 @@ function PricingGrids() {
         {/* Card 2 */}
         <div className="rounded-xl bg-slate-700 text-white">
           {/* upper card container */}
-          <div className="mx-2 mt-2 bg-blue-950 px-2">
-            <h3>basic</h3>
-            <span>100GB</span>
-            <p>$1.99/month</p>
-            <a href="#">Purchase</a>
+          <div className="mx-2 mt-2 flex flex-col items-center justify-center rounded-t-xl bg-blue-950 p-8">
+            <div className="mb-10 uppercase">basic</div>
+            <span className="mb-2 font-serif text-5xl">100GB</span>
+            <p className="mb-4 font-light">$1.99/month</p>
+            <a
+              href="#"
+              className="my-6 inline-block rounded-lg border border-violet-600 px-10 py-3 text-center duration-200 hover:border-violet-800 hover:bg-violet-800"
+            >
+              Purchase
+            </a>
           </div>
           {/* Border */}
           <div className="border-t border-slate-700"></div>
           {/* lower card container */}
           <div className="mx-2 mb-2 bg-blue-950 p-4">
             {/* List Container */}
-            <div>
+            <div className="space-y-2">
               {/* List Item 1 */}
-              <div className="flex justify-center">
+              <div className="flex justify-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -103,10 +113,10 @@ function PricingGrids() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M5 12l5 5l10 -10" />
                 </svg>
-                <span className="ml-1 text-sm">100 GB of storage</span>
+                <span className="text-sm">100 GB of storage</span>
               </div>
               {/* List Item 2 */}
-              <div className="flex justify-center">
+              <div className="flex justify-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -120,10 +130,10 @@ function PricingGrids() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M5 12l5 5l10 -10" />
                 </svg>
-                <span className="ml-1 text-sm">100 GB of storage</span>
+                <span className="text-sm">Option to add members</span>
               </div>
               {/* List Item 3 */}
-              <div className="flex justify-center">
+              <div className="flex justify-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -137,7 +147,7 @@ function PricingGrids() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M5 12l5 5l10 -10" />
                 </svg>
-                <span className="ml-1 text-sm">100 GB of storage</span>
+                <span className="text-sm">Extra member benefits</span>
               </div>
             </div>
           </div>
@@ -145,20 +155,25 @@ function PricingGrids() {
         {/* Card 3 */}
         <div className="rounded-xl bg-slate-700 text-white">
           {/* upper card container */}
-          <div className="mx-2 mt-2 bg-blue-950 px-2">
-            <h3>basic</h3>
-            <span>100GB</span>
-            <p>$1.99/month</p>
-            <a href="#">Purchase</a>
+          <div className="mx-2 mt-2 flex flex-col items-center justify-center rounded-t-xl bg-blue-950 p-8">
+            <div className="mb-10 uppercase">basic</div>
+            <span className="mb-2 font-serif text-5xl">100GB</span>
+            <p className="mb-4 font-light">$1.99/month</p>
+            <a
+              href="#"
+              className="my-6 inline-block rounded-lg border border-violet-600 px-10 py-3 text-center duration-200 hover:border-violet-800 hover:bg-violet-800"
+            >
+              Purchase
+            </a>
           </div>
           {/* Border */}
           <div className="border-t border-slate-700"></div>
           {/* lower card container */}
           <div className="mx-2 mb-2 bg-blue-950 p-4">
             {/* List Container */}
-            <div>
+            <div className="space-y-2">
               {/* List Item 1 */}
-              <div className="flex justify-center">
+              <div className="flex justify-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -172,10 +187,10 @@ function PricingGrids() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M5 12l5 5l10 -10" />
                 </svg>
-                <span className="ml-1 text-sm">100 GB of storage</span>
+                <span className="text-sm">100 GB of storage</span>
               </div>
               {/* List Item 2 */}
-              <div className="flex justify-center">
+              <div className="flex justify-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -189,10 +204,10 @@ function PricingGrids() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M5 12l5 5l10 -10" />
                 </svg>
-                <span className="ml-1 text-sm">100 GB of storage</span>
+                <span className="text-sm">Option to add members</span>
               </div>
               {/* List Item 3 */}
-              <div className="flex justify-center">
+              <div className="flex justify-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -206,7 +221,7 @@ function PricingGrids() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M5 12l5 5l10 -10" />
                 </svg>
-                <span className="ml-1 text-sm">100 GB of storage</span>
+                <span className="text-sm">Extra member benefits</span>
               </div>
             </div>
           </div>
