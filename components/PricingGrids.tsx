25afaec796a4ef48f74c31b4718960edd03dd992
Impulse <impulses4mini@gmail.com>
2025-03-14 10:37:51 +0300

    add cards

--- components/PricingGrids.tsx
+++ components/PricingGrids.tsx
@@ -1,4 +1,218 @@
 function PricingGrids() {
-  return <div>PricingGrids</div>;
+  return (
+    //Background Container
+    <div className="flex min-h-screen items-center justify-center bg-blue-950">
+      {/* Cards Container */}
+      <div className="flex flex-col md:flex-row">
+        {/* Card 1*/}
+        <div className="rounded-xl bg-slate-700 text-white">
+          {/* upper card container */}
+          <div className="mx-2 mt-2 bg-blue-950 px-2">
+            <h3>basic</h3>
+            <span>100GB</span>
+            <p>$1.99/month</p>
+            <a href="#">Purchase</a>
+          </div>
+          {/* Border */}
+          <div className="border-t border-slate-700"></div>
+          {/* lower card container */}
+          <div className="mx-2 mb-2 bg-blue-950 p-4">
+            {/* List Container */}
+            <div>
+              {/* List Item 1 */}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className="ml-1 text-sm">100 GB of storage</span>
+              </div>
+              {/* List Item 2 */}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className="ml-1 text-sm">100 GB of storage</span>
+              </div>
+              {/* List Item 3 */}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className="ml-1 text-sm">100 GB of storage</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Card 2 */}
+        <div className="rounded-xl bg-slate-700 text-white">
+          {/* upper card container */}
+          <div className="mx-2 mt-2 bg-blue-950 px-2">
+            <h3>basic</h3>
+            <span>100GB</span>
+            <p>$1.99/month</p>
+            <a href="#">Purchase</a>
+          </div>
+          {/* Border */}
+          <div className="border-t border-slate-700"></div>
+          {/* lower card container */}
+          <div className="mx-2 mb-2 bg-blue-950 p-4">
+            {/* List Container */}
+            <div>
+              {/* List Item 1 */}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className="ml-1 text-sm">100 GB of storage</span>
+              </div>
+              {/* List Item 2 */}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className="ml-1 text-sm">100 GB of storage</span>
+              </div>
+              {/* List Item 3 */}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className="ml-1 text-sm">100 GB of storage</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Card 3 */}
+        <div className="rounded-xl bg-slate-700 text-white">
+          {/* upper card container */}
+          <div className="mx-2 mt-2 bg-blue-950 px-2">
+            <h3>basic</h3>
+            <span>100GB</span>
+            <p>$1.99/month</p>
+            <a href="#">Purchase</a>
+          </div>
+          {/* Border */}
+          <div className="border-t border-slate-700"></div>
+          {/* lower card container */}
+          <div className="mx-2 mb-2 bg-blue-950 p-4">
+            {/* List Container */}
+            <div>
+              {/* List Item 1 */}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className="ml-1 text-sm">100 GB of storage</span>
+              </div>
+              {/* List Item 2 */}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className="ml-1 text-sm">100 GB of storage</span>
+              </div>
+              {/* List Item 3 */}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className="ml-1 text-sm">100 GB of storage</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default PricingGrids;
